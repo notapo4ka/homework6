d5183fef73086d3b0da5735078115e90590ea74b
Dmytro Pochuiev <dmytro.pochuiev@gmail.com>
2023-03-09 00:32:54 +0200

    Added .java files

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+
+    public void start() {
+
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startElectricity() {}
+
+    private void startCommand() {}
+
+    private void startFuelSystem() {}
+}
